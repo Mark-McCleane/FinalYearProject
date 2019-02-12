b55fd36bbdde18b9db81afd44fef762db0d06215
Mark McCleane <mccleanemark177@gmail.com>
2019-02-12 18:59:58 +0000

    Added Buttons

---
+++ app/src/main/java/com/example/samapp/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.samapp;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import java.io.IOException;
+
+    public class MainActivity extends AppCompatActivity {
+        private Button play, stop, record;
+        private MediaRecorder myAudioRecorder;
+        private String outputFile;
+        private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+        private boolean permissionToRecordAccepted = false;
+        private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+        @Override
+        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            switch (requestCode){
+                case REQUEST_RECORD_AUDIO_PERMISSION:
+                    permissionToRecordAccepted  = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    break;
+            }
+            if (!permissionToRecordAccepted ) finish();
+
+        }
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            play = (Button) findViewById(R.id.play);
+            stop = (Button) findViewById(R.id.stop);
+            record = (Button) findViewById(R.id.record);
+            stop.setEnabled(false);
+            play.setEnabled(false);
+            /*outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording.3gp";
+            myAudioRecorder = new MediaRecorder();
+//            myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+            myAudioRecorder.setOutputFile(outputFile);
+
+            record.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    try {
+                        myAudioRecorder.prepare();
+                        myAudioRecorder.start();
+                    } catch (IllegalStateException ise) {
+                        // make something ...
+                    } catch (IOException ioe) {
+                        // make something
+                    }
+                    record.setEnabled(false);
+                    stop.setEnabled(true);
+                    Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_LONG).show();
+                }
+            });
+
+            stop.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    myAudioRecorder.stop();
+                    myAudioRecorder.release();
+                    myAudioRecorder = null;
+                    record.setEnabled(true);
+                    stop.setEnabled(false);
+                    play.setEnabled(true);
+                    Toast.makeText(getApplicationContext(), "Audio Recorder successfully", Toast.LENGTH_LONG).show();
+                }
+            });
+
+            play.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    MediaPlayer mediaPlayer = new MediaPlayer();
+                    try {
+                        mediaPlayer.setDataSource(outputFile);
+                        mediaPlayer.prepare();
+                        mediaPlayer.start();
+                        Toast.makeText(getApplicationContext(), "Playing Audio", Toast.LENGTH_LONG).show();
+                    } catch (Exception e) {
+                        // make something
+                    }
+                }
+            });        }
+*/
+    }}
